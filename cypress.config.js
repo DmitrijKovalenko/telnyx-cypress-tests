2c1820843a8e9f7ff3d19f3f67ddf4fcbb7d984b
Dima Kovalenko <dmitrijkovalenko86@gmail.com>
2025-07-06 19:02:49 +0300

    Move .github/workflows to root directory

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'k3gnfc',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
